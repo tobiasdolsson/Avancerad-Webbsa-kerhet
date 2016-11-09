8a2e934922bfa4ec99f6805c8a1f18172a16bfae
mrsvedberg <johan@svedberg.biz>
2016-11-09 11:26:39 +0100

    added nbrtests

--- src/hemuppgift1_B2/Main.java
+++ src/hemuppgift1_B2/Main.java
@@ -5,17 +5,19 @@ public class Main {
 	public static void main(String[] args){
 		int test = 0;
 		int deviation = 0;
+		int nbrtests = 10000;
 		
 		//Om man nollställer cs i calculateMean behöver man inte skapa tusen objekt lol men orkar inte ändra
 		
-		for(int i=0; i<1000; i++){
-			CoinSimulator cs = new CoinSimulator(16,7,1);
+		for(int i=0; i<nbrtests; i++){
+			CoinSimulator cs = new CoinSimulator(20,7,1);
 			test += cs.calculateMean();
 			//deviation += cs.calculateDeviation(322);
 			//cs.visualizeArray();
+			System.out.println(i);
 		}
 		
-		System.out.println(test/1000);
+		System.out.println(test/nbrtests);
 		System.out.println(322-(test/1000));
 		//System.out.println(deviation/1000);		
 		//System.out.println(cs.calculateMean());
