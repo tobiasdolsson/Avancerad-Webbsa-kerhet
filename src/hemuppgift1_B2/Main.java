3d643933a0d56f603b994d6d924f7cd6ec5a7d3c
tobiasdolsson <Tobias@Tobiass-MacBook-Air.local>
2016-11-09 11:29:45 +0100

    Lambda added
    added lambda

--- src/hemuppgift1_B2/Main.java
+++ src/hemuppgift1_B2/Main.java
@@ -5,11 +5,20 @@ public class Main {
 	public static void main(String[] args){
 		int test = 0;
 		int deviation = 0;
+
 		int nbrtests = 10000;
+		double lambda = 3.66;
 		
 		//Om man nollställer cs i calculateMean behöver man inte skapa tusen objekt lol men orkar inte ändra
 		
 		for(int i=0; i<nbrtests; i++){
+
+		
+		
+		//Om man nollställer cs i calculateMean behöver man inte skapa tusen objekt lol men orkar inte ändra
+		
+		
+
 			CoinSimulator cs = new CoinSimulator(20,7,1);
 			test += cs.calculateMean();
 			//deviation += cs.calculateDeviation(322);
@@ -17,8 +26,10 @@ public class Main {
 			System.out.println(i);
 		}
 		
+
 		System.out.println(test/nbrtests);
-		System.out.println(322-(test/1000));
+
+
 		//System.out.println(deviation/1000);		
 		//System.out.println(cs.calculateMean());
 		//cs.visualizeArray();
