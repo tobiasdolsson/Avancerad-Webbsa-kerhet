465d8ab7ec34953a056c2b6795c4dc656ba582d4
tobiasdolsson <Tobias@Tobiass-MacBook-Air.local>
2016-11-09 18:19:41 +0100

    Iteration calculation improvement
    No longer need to enter number of iterations. Program stops when c.i
    width is within the given interval.

--- src/hemuppgift1_B2/CoinSimulator.java
+++ src/hemuppgift1_B2/CoinSimulator.java
@@ -5,68 +5,68 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class CoinSimulator {
-	
+
 	public int u, k, c, currentCoins, numberOfThrows, mean, n;
 	public double variance, deviation, width;
 	public double lambda;
 	int[] bins;
 	public ArrayList<Integer> data;
-	
-	public CoinSimulator(int u, int k, int c){
+
+	public CoinSimulator(int u, int k, int c) {
 		this.u = u;
 		this.k = k;
 		this.c = c;
 		lambda = 3.66;
 		data = new ArrayList<Integer>();
-		
+
 	}
-	
-	public int calculateMean(){
-		bins = new int[(int) Math.pow(2,u)];
+
+	public int calculateMean() {
+		bins = new int[(int) Math.pow(2, u)];
 		currentCoins = 0;
 		numberOfThrows = 0;
-		
-		
+
 		Random rand = new Random();
 		int currentBin;
-		//System.out.print(rand.nextInt(bins.length));
-				
-		while(currentCoins < c){
-			
+
+		while (currentCoins < c) {
+
 			currentBin = rand.nextInt(bins.length);
 			bins[currentBin]++;
-			if(bins[currentBin]==k){
+			if (bins[currentBin] == k) {
 				currentCoins++;
 			}
-			numberOfThrows++;		
+			numberOfThrows++;
 		}
 		data.add(numberOfThrows);
 		return numberOfThrows;
 	}
-	
-	public double calculateCIWidth(){
+
+	public double calculateCIWidth() {
+		variance = 0;
+		deviation = 0;
 		mean = 0;
 		n = data.size();
-		for(int i=0; i<n; i++){
+		for (int i = 0; i < n; i++) {
 			mean += data.get(i);
 		}
-		mean = mean/n;
-		System.out.println("mean throws: "+mean);
-		for(int i: data){
-			variance += ((i-mean)*(i-mean));
+		mean = mean / n;
+		for (int i : data) {
+			variance += ((i - mean) * (i - mean));
 		}
-			
-			variance = variance/data.size();
-			deviation = Math.sqrt(variance);
-		return 2 * lambda * (deviation/Math.sqrt(n));
+
+		variance = variance / data.size();
+		deviation = Math.sqrt(variance);
+		return 2 * lambda * (deviation / Math.sqrt(n));
 	}
-	
-	public int calculateVariance(int mean, int n){
-		return ((numberOfThrows-mean)*(numberOfThrows-mean))/n;
+
+	public void anounceMean() {
+		System.out.println("final mean throws: " + mean);
 	}
-	//Debug purposes
-	public void visualizeArray(){
-		System.out.println(Arrays.toString(bins));		
+
+	// Debug purposes
+	public void visualizeArray() {
+		System.out.println(Arrays.toString(bins));
 	}
-	
+
 }
--- src/hemuppgift1_B2/Main.java
+++ src/hemuppgift1_B2/Main.java
@@ -1,18 +1,33 @@
 package hemuppgift1_B2;
 
 public class Main {
-	
-	public static void main(String[] args){
-	
-		int nbrtests = 200;
-		
-		CoinSimulator cs = new CoinSimulator(20,7,1);
-		
-		for(int i=0; i<nbrtests; i++){
+
+	public static void main(String[] args) {
+
+		int nbrOfTests = 0;
+		int minIterations = 10;
+		double currentCI;
+		int ci = 24;
+
+		CoinSimulator cs = new CoinSimulator(16, 2, 100);
+
+		while (true) {
+
 			cs.calculateMean();
-			System.out.println(i);
+			currentCI = cs.calculateCIWidth();
+			nbrOfTests++;
+			//System.out.println("Current width: "+currentCI);			
+			//System.out.println("Number of iterations :" + nbrOfTests);
+			if (currentCI != 0 && currentCI < ci && nbrOfTests > minIterations) {
+				System.out.println("Number of iterations :" + nbrOfTests);
+				break;
+			}
+
 		}
-		System.out.println("c.i width: "+cs.calculateCIWidth());
-		}
+
+		System.out.println("c.i width: " + cs.calculateCIWidth());
+		cs.anounceMean();
+
+	}
 
 }
